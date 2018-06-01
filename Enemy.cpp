48d2cb4627cd90b411eb0e6e18480c068b95dd07
Michał Parafiniuk <michal.a.parafiniuk@gmail.com>
2018-06-01 18:21:29 +0200

    Behold, the Enemy

---
+++ Enemy.cpp
@@ -0,0 +1,13 @@
+#include "Enemy.h"
+
+Enemy::Enemy() {}
+
+Enemy::Enemy(sf::Sprite* _sprite){
+    sprite = _sprite;
+}
+
+void Enemy::update(){
+    if (health <= 0) alive = false;
+
+    //... 'ere be dragons
+}
---
+++ Enemy.h
@@ -0,0 +1,14 @@
+#ifndef __ENEMY_H__
+#define __ENEMY_H__
+
+#include "Pawn.h"
+#include <SFML/Graphics.hpp>
+
+class Enemy : public Pawn{
+public:
+    Enemy();
+    Enemy(sf::Sprite* sprite);
+    void update();
+};
+
+#endif
